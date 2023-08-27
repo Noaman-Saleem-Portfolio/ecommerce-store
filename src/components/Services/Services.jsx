ef215e36d1fcdee5c461f0ba2b966700c6121b14
Noaman-Saleem-Portfolio <noaman2266@gmail.com>
2023-08-28 01:08:45 +0500

    Started services

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { TbTruckDelivery } from "react-icons/tb";
+import { MdSecurity } from "react-icons/md";
+import { GiReceiveMoney } from "react-icons/gi";
+import { RiSecurePaymentLine } from "react-icons/ri";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import styles from "./Services.module.css";
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div className={`container ${styles.services}`}>
+      <Row className="g-2">
+        <Col md={3}>
+          <TbTruckDelivery />
+          <p>Super Fast and Free Delivery</p>
+        </Col>
+        <Col md={3}>
+          <Row className="gy-2">
+            <Col md={12}>
+              <GiReceiveMoney />
+              <p>Non-contact Shipping</p>
+            </Col>
+            <Col md={12}>
+              <MdSecurity />
+              <p>Money-back Guaranty</p>
+            </Col>
+          </Row>
+        </Col>
+        <Col md={3}>
+          <RiSecurePaymentLine />
+          <p>Super Secure Payment System</p>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default Services;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 // import Hero from "../../components/Hero/Hero";
 import HeroBootstrap from "../../components/Hero/HeroBootstrap";
+import Services from "../../components/Services/Services";
 
 const Home = () => {
   return (
     <>
       <HeroBootstrap introText="Store" />
+      <Services />
     </>
   );
 };
